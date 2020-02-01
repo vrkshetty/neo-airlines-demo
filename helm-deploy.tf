cc3429ac0c0e7a5a0740be0d86e23b8f1a5a4c46
vrkshetty <vijayrkshetty@gmail.com>
2020-02-01 22:26:19 +0000

    Service account creation manifest addded

--- helm-deploy.tf
+++ helm-deploy.tf
@@ -1,6 +1,11 @@
 resource "null_resource" "helm_deploy" {
   provisioner "local-exec" {
     command = <<EOT
+    kubectl apply -f manual-task/deploy-bot-sa.yaml
+EOT
+  }
+  provisioner "local-exec" {
+    command = <<EOT
     helm install --namespace tools --name cicd stable/jenkins --values helm-config/jenkins-values.yaml
 EOT
   }
